ef33b0b299859c432593d5b3dbb965c96d24c179
TAE YONG <139419000+94TEO@users.noreply.github.com>
2023-09-22 12:14:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 289 ms, Memory: 65,420 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,18 +1,18 @@
-def f(month, cost):
+def f(month, amount):
     global total
     if month >= 12:
-        total = min(total, cost)
+        total = min(total, amount)
         return
-    if cost > total:
+    if amount > total:
         return
-    f(month + 1, cost + M[month] * P[0])
-    f(month + 1, cost + P[1])
-    f(month + 3, cost + P[2])
-    f(month + 12, cost + P[3])
+    f(month + 1, amount + M[month] * cost[0])
+    f(month + 1, amount + cost[1])
+    f(month + 3, amount + cost[2])
+    f(month + 12, amount + cost[3])
 
 
 for test_case in range(int(input())):
-    P = tuple(map(int, input().split()))
+    cost = tuple(map(int, input().split()))
     M = tuple(map(int, input().split()))
     total = 3000*365 + 1
     f(0, 0)
