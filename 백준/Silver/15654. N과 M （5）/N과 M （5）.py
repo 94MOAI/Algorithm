53db42f8c085725a62c3a0c0c1c66f3248dc7857
TAE YONG <139419000+94TEO@users.noreply.github.com>
2023-09-25 01:21:28 +0900

    [Silver III] Title: N과 M (5), Time: 188 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,23 @@
+# 순열
+def f(depth):
+    if depth == M:
+        print(*subset)
+        return
+    for i in range(N):
+        if used[i]:
+            continue
+        subset.append(arr[i])
+        used[i] = 1
+        f(depth+1)
+        subset.pop()
+        used[i] = 0
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+used = [0]*N
+subset = []
+
+f(0)
