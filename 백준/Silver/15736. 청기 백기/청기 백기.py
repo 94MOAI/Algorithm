e09b6d5e29126fed12011cc9618a24e0eee5f7cd
TAE YONG <139419000+94MOAI@users.noreply.github.com>
2023-11-07 20:38:31 +0900

    [Silver IV] Title: 청기 백기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15736. 청기 백기/청기 백기.py
+++ 백준/Silver/15736. 청기 백기/청기 백기.py
@@ -1,12 +1,1 @@
-N = int(input())
-number = 0
-answer = 0
-while True:
-    number += 1
-    if number**2 <= N:
-        answer +=1
-        continue
-    else:
-        break
-
-print(answer)
+print(int(int(input())**0.5))
