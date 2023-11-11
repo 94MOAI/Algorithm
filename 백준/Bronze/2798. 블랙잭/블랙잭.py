9c41d977403e08bca196e644f440c24780a0e2b0
TAE YONG <139419000+94MOAI@users.noreply.github.com>
2023-11-11 20:33:12 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort(reverse=True)
+mx = 0
+
+
+for i in range(len(nums) - 2):
+    for j in range(i + 1, len(nums) - 1):
+        for k in range(j + 1, len(nums)):
+            total = nums[i] + nums[j] + nums[k]
+            if total <= M:
+                mx = max(mx, total)
+
+print(mx)
